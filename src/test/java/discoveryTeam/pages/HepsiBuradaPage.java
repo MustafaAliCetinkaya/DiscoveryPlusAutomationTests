f359ec2c3535ec2d2d3c5426f60f4e9a9447d84f
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-14 20:46:14 +0300

    HepsiBurada Test Cases are in progress

---
+++ src/test/java/discoveryTeam/pages/HepsiBuradaPage.java
@@ -0,0 +1,31 @@
+package discoveryTeam.pages;
+
+import discoveryTeam.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+public class HepsiBuradaPage {
+    public HepsiBuradaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(css = "button#onetrust-accept-btn-handler")
+    public WebElement acceptCookies;
+    @FindBy(css = "a#login")
+    public WebElement homaPageLoginButton;
+    @FindBy(css = "input#txtUserName")
+    public WebElement emailBox;
+    @FindBy(css = "input#txtEmail")
+    public WebElement emailSignupBox;
+    @FindBy(css = "button#btnSignUpSubmit")
+    public WebElement continueSignupButton;
+    @FindBy(css = "button#btnLogin")
+    public WebElement loginButton;
+    @FindBy(xpath = "//span[contains(text(),'Giriş Yap')]")
+    public WebElement homaPageLoginDropdown;
+    @FindBy(xpath = "//div[contains(text(),'Üye ol')]")
+    public WebElement signupButton;
+
+}
