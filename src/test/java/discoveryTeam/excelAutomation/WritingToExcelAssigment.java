e5c4ae0cbb42bd629384acafb43c0c8993149b8f
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-16 10:07:18 +0300

    Excel Automation tests passes successfully

--- src/test/java/discoveryTeam/excelAutomation/WritingToExcelAssigment.java
+++ src/test/java/discoveryTeam/excelAutomation/WritingToExcelAssigment.java
@@ -12,22 +12,20 @@ import java.io.IOException;
 public class WritingToExcelAssigment {
     @Test
     public void writingToExcelFile() throws IOException {
-        String path = "src/test/resources/book.xlsx"; //Java can open the Excel files but can not read. File do not has to be in the resource folder, it can read from directly computer as well.
+        String path = "/Users/mustafacetinkaya/IdeaProjects/BasicNavigationTests/src/test/resources/Book2.xlsx"; //Java can open the Excel files but can not read. File do not has to be in the resource folder, it can read from directly computer as well.
 
-        FileInputStream file = new FileInputStream(path);//For opening
+        FileInputStream fileInputStream = new FileInputStream(path);//For opening
 
         //Read the Excel file
-        Workbook excel = WorkbookFactory.create(file);
+        Workbook excel = WorkbookFactory.create(fileInputStream);
 
         FileOutputStream fileOutputStream = new FileOutputStream(path);//For reading after saving. Order is important. Firstly open, then read, lastly show result
-
 
         //Select sheet that will be written
         Sheet sheet = excel.getSheetAt(0);   //First sheet's index is 0.
 
-
         //Select cell (column) and row that will be written (Headers)
-        sheet.getRow(0).getCell(1).setCellValue("Name");
+        sheet.getRow(1).getCell(1).setCellValue("Name");
         sheet.getRow(0).getCell(2).setCellValue("Surname");
         sheet.getRow(0).getCell(3).setCellValue("Batch Number");
         sheet.getRow(0).getCell(4).setCellValue("Group");
@@ -41,7 +39,7 @@ public class WritingToExcelAssigment {
 
         excel.write(fileOutputStream);
         excel.close();
-        file.close();
+        fileInputStream.close();
 
     }
 }
